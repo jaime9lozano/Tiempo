4ff5481f4c5348b7bb5237123681da6a50ab0372
KevinSanchez5 <kjesussb@gmail.com>
2023-09-25 20:56:14 +0200

    version 0.2

--- src/main/java/modelos/Clima.java
+++ src/main/java/modelos/Clima.java
@@ -1,4 +1,14 @@
 package modelos;
 
+import java.time.LocalTime;
+
 public class Clima {
+    String localidad;
+    String provincia;
+    double tempMax;
+    LocalTime horaTMax;
+    double tempMin;
+    LocalTime horaTemMin;
+    boolean precipitacion;
+
 }
