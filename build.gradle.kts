e51bf8dc61b1e340cad1481fe330ca1a8e8a1115
KevinSanchez5 <kjesussb@gmail.com>
2023-09-25 20:47:54 +0200

    version 0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // h2
+    implementation("com.h2database:h2:2.2.224")
+    // Lombok
+    implementation("org.projectlombok:lombok:1.18.28")
+    testImplementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    // Logger
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    // Ibatis para leer los scripts SQL desde archivos
+    implementation("org.mybatis:mybatis:3.5.13")
 }
 
 tasks.test {
