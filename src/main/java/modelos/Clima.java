80824fae713bad0b9ba1fe17c2f778659501a926
KevinSanchez5 <115721589+KevinSanchez5@users.noreply.github.com>
2023-09-26 02:31:45 +0200

    version 0.4 manito

--- src/main/java/modelos/Clima.java
+++ src/main/java/modelos/Clima.java
@@ -10,5 +10,9 @@ public class Clima {
     double tempMin;
     LocalTime horaTemMin;
     boolean precipitacion;
-
+    UUID codigo;
+// he tenido que crear el constructor para que el CRUD me vaya, si quieres o sabes
+    //como hacerlo no dudes en cambiarlo
+    public Clima(UUID codigo, String localidad, String provincia, double temMax, double temMin, Time horaTMax, Time horaTemMin, boolean precipitacion) {
+    }
 }
